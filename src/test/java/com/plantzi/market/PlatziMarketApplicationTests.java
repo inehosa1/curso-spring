5c1320d607ec0bba9ff157acfe4e262cb75eb284
ricardo <chanix1998@gmail.com>
2022-09-15 14:24:50 -0500

    first commit

---
+++ src/test/java/com/plantzi/market/PlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantzi.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
